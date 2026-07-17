a6e72f308772cbd74e420c3709a70435a51f3062
Nusrath Jahan <nusrathj152@gmail.com>
2026-07-17 12:12:37 +0530

    Use PORT env variable for Render

--- app.py
+++ app.py
@@ -23,4 +23,5 @@ def process_audio():
     return send_file(output_path, mimetype="audio/mp3")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
